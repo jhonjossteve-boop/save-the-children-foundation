8b6dde1cf67a0ac5c9fd0d8d5885fe499fad6cb2
jhonjossteve-boop <jhonjossteve@gmail.com>
2025-10-23 08:45:24 -0700

    Delete client directory

--- client/src/components/examples/DonationPanel.tsx
+++
@@ -1,9 +0,0 @@
-import DonationPanel from '../DonationPanel';
-
-export default function DonationPanelExample() {
-  return (
-    <DonationPanel
-      onDonate={(amount) => console.log(`Donating $${amount}`)}
-    />
-  );
-}
